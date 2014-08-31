7b5be6c51601bbddc298cdbb7ec85783c773cf5c
David Jarvis <david@standardtreasury.com>
2014-08-30 22:08:38 -0700

    [api] Initial API commit for testing purposes

---
+++ src/darg/api/v1.clj
@@ -0,0 +1,7 @@
+(ns darg.api.v1)
+
+(defn parse-forwarded-email
+  "Parse an e-mail that has been forwarded by Mailgun"
+  [body]
+  (println body)
+  )
--- src/darg/core.clj
+++ src/darg/core.clj
@@ -1,13 +1,16 @@
 (ns darg.core
   (:gen-class) ;; need this for the main method
-  (:require [compojure.core :refer [defroutes GET]]
-            [ring.adapter.jetty :as ring]
+  (:require [compojure.core :refer [defroutes GET POST]]
             [lobos.core :as lobos]
             [lobos.config :as lconfig]
-            ))
+            [ring.adapter.jetty :as ring]
+
+            [darg.api.v1 :as api]))
 
 (defroutes routes
-  (GET "/" [] "<h2>Hello World</h2>"))
+  (GET "/" [] "<h2>Hello World</h2>")
+  (POST "/api/v1/email/" x api/parse-forwarded-email)
+  )
 
 (defn -main []
   (lconfig/init)
