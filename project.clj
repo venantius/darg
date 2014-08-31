909cab6f453b6e4cdf2d36d43ff2a62017d844a7
David Jarvis <david@standardtreasury.com>
2014-08-30 18:08:17 -0700

    [project] Adding :min-lein-version to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject darg "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :main darg.core
+  :min-lein-version "2.0.0"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
