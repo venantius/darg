778dab3fd785e35d31746a310b73b0d54d4d17ad
johnofburnham <savelago@gmail.com>
2014-09-01 14:33:58 -0700

    Set-korma-db before tests

--- test/darg/test_db.clj
+++ test/darg/test_db.clj
@@ -12,17 +12,20 @@
   (is korma/_default))
 
 (deftest we-can-insert-into-the-db
+	(db/set-korma-db)
 	(insert users (values{:id 99, :email "domo@test.com", :username "arrigato"}))
 	(is (= "arrigato" (:username (first (select users (where {:id 99}))))))
   )
 
 (deftest we-can-update-in-the-db
+	(db/set-korma-db)
        (insert users (values {:id 98, :email "domo2@test.com", :username "otakon"}))
 	(update users (set-fields {:username "irrashaimase"}) (where {:id 98}))
 	(is (= "irrashaimase" (:username (first (select users (where {:id 98}))))))
 )
 
 (deftest we-can-delete-from-the-db
+	(db/set-kroma-db)
 	(delete users (where{:id 99}))
 	(is (= nil (first (select users (where {:id 99})))))
 )
