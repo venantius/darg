64139c884f47145ea9af7eefdf89d3345dea697d
johnofburnham <savelago@gmail.com>
2014-09-05 23:56:26 -0700

    test delete a user without tasks (for now)

--- test/darg/db_test.clj
+++ test/darg/db_test.clj
@@ -23,5 +23,5 @@
   (is (= "irrashaimase" (:username (first (select users (where {:id 3})))))))
 
 (deftest we-can-delete-from-the-db
-	(delete users (where{:id 2}))
-	(is (= nil (first (select users (where {:id 2}))))))
+	(delete users (where{:id 3}))
+	(is (= nil (first (select users (where {:id 3}))))))
