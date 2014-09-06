352faca7ec4d6519ff5614d44ad9aa448c1b9ed8
johnofburnham <savelago@gmail.com>
2014-09-06 14:26:50 -0700

    email-parse splits tasks by newline

--- test/darg/api/v1_test.clj
+++ test/darg/api/v1_test.clj
@@ -3,7 +3,7 @@
 
 (def test-received-params
   ;; this is an example of what we actually get forwarded to us from Mailgun
-  {:stripped-html "<p>Dancing tiem!!</p>"
+  {:stripped-html "<p>Dancing tiem!!</p><p>Aint it a thing?</p>"
    :From "butts@darg.io"
    :message-headers [["Received", "by luna.mailgun.net with HTTP; Tue, 02 Sep 2014 01:51:29 +0000",]
                      ["Mime-Version", "1.0"],
@@ -16,7 +16,8 @@
    :stripped-signature ""
    :signature "ad94075a8d99b540f4cb4aa0847eb49328bde219a67fb5639448b559a1b92102"
    :recipient "test.api@darg.io"
-   :stripped-text "Dancing tiem!!"
+   :stripped-text "Dancing tiem!!
+   Aint it a thing?"
    :Subject "Let's dance mofo"
    :Mime-Version 1.0
    :token "ee55af9ce04725e2b93ca5844b14621ac96de7e9144b21222f"
@@ -29,7 +30,8 @@
    :timestamp 1409622691
    :Content-Type "text/plain; charset=\"ascii\""
    :subject "Let's dance mofo"
-   :body-plain "Dancing tiem!!"})
+   :body-plain "Dancing tiem!!
+   Aint it a thing?"})
 
 (deftest email-sent-to-us-is-parseable
   (is (= 1 1))
