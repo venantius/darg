9685ab6d964572e8360ee896bea2f0e7cbdb94c2
johnofburnham <savelago@gmail.com>
2014-09-05 20:26:14 -0700

    Added function to insert db fixture data

--- src/darg/db-util.clj
+++ src/darg/db-util.clj
@@ -9,14 +9,6 @@
 (def table-list [users teams tasks])
 (def fixture-map (parse-string (slurp fixture-yaml)))
 
-(defn reset-db
-"Resets db: Deletes all data, applies fixtures"
-  ((
-	(for [x table-list] (
-		(delete x) 
-		(insert x (values ((keyword (:name x)) (parse-string(slurp fixture-yaml))))))
-	)
-)
 
 (defn add-to-users
 [x params]
