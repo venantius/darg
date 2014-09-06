6b40595057dfe6389ca133a47280536fdb12e3c8
johnofburnham <savelago@gmail.com>
2014-09-06 00:08:27 -0700

    dates now use clj-time
    dates must be coerced into sql dates using clj-time.coerce/to-sql-date

--- test/darg/fixtures/model.clj
+++ test/darg/fixtures/model.clj
@@ -2,6 +2,7 @@
 	(:require [clj-time.core :as t]
 		   [darg.db-util :as db-util])
 	(:use [korma.core]
+		[clj-time.coerce :as c :only [to-sql-date]]
 	         [darg.model])
 	)
 
@@ -33,13 +34,13 @@
 
 (def task-test-1
 	[{ :id 1
-	   :date (java.sql.Date. 2012 2 16)
+	   :date (c/to-sql-date (t/date-time 2012 2 16))
                :user-id 1
                :team-id 1
                :task "Do a good deed everyday"
              }
              {  :id 2
-                :date (java.sql.Date. 2012 2 16)
+                :date (c/to-sql-date (t/date-time 2012 2 16))
                 :user-id 2
                 :team-id 2
                 :task "Destroy all humans"
