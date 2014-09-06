17ba54138ec1d13465531e551e4ea547edb56810
johnofburnham <savelago@gmail.com>
2014-09-06 16:14:12 -0700

    parse date from subject string
    Extract date from subject string
    Convert date to SQL compatible date format

--- src/darg/api/v1.clj
+++ src/darg/api/v1.clj
@@ -1,6 +1,7 @@
 (ns darg.api.v1
   (:require [clojure.tools.logging :as logging]
-    [clojure.string :only [split] ]))
+    [clojure.string :only [split] ]
+    [darg.db-util :refer :all]))
 
 ;; our logging problem is very similar to https://github.com/iphoting/heroku-buildpack-php-tyler/issues/17
 (defn parse-forwarded-email
@@ -16,9 +17,12 @@
     (logging/info "Full Mailgun POST: " body)
     (str params)))
 
-(defn email-parse
+(defn parse-email
   [email]
     (let [tasks (clojure.string/split (:body-plain email) #"\n   ")]
       (into (empty [])
         (for [task tasks] 
-          {:user-email (:from email) :team-email (:recipient email) :date (:subject email) :task task}))))
+          { :user-email (:from email) 
+            :team-email (:recipient email) 
+            :date (sql-date-from-subject (:subject email)) 
+            :task task}))))
--- src/darg/db_util.clj
+++ src/darg/db_util.clj
@@ -1,19 +1,32 @@
 (ns darg.db-util
 	(:use [korma.core :as korma]
                      [darg.model :refer :all]
+                     [clj-time.format :as f]
+                     [clj-time.coerce :as c]
+                     [clj-time.core :as t]
 		  )
 	)
 
 
 (defn get-userid 
 	[field value]
-	(select users (fields :id) (where {(keyword field) value}))
-)
+	(select users (fields :id) (where {(keyword field) value})))
 
 (defn get-teamid
 	[field value]
-	(select teams (fields :id) (where {(keyword field) value}))
-)
+	(select teams (fields :id) (where {(keyword field) value})))
+
+; (defn date-extract
+; 	[string]
+; 	(re-find (re-pattern "(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\\s\\d{2}\\s\\d{4}") string))
+
+(defn sql-date-from-subject
+	[string]
+	(c/to-sql-date (f/parse 
+		(f/formatter "MMM dd YYY") 
+		(re-find (re-pattern "(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\\s\\d{2}\\s\\d{4}") string))))
+
+
 
 ; (defn add-teammembers
 ; 	[teamid [userids]]
