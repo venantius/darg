6123d555921a61b3d82629d8b5836a0fd6f1f5e3
johnofburnham <savelago@gmail.com>
2014-09-06 14:26:50 -0700

    email-parse splits tasks by newline

--- src/darg/api/v1.clj
+++ src/darg/api/v1.clj
@@ -1,12 +1,13 @@
 (ns darg.api.v1
-  (:require [clojure.tools.logging :as logging]))
+  (:require [clojure.tools.logging :as logging]
+    [clojure.string :only [split] ]))
 
 ;; our logging problem is very similar to https://github.com/iphoting/heroku-buildpack-php-tyler/issues/17
 (defn parse-forwarded-email
   "Parse an e-mail that has been forwarded by Mailgun"
   [body]
   (let [params (:params body)
-        {:keys [recipient sender from subject
+        {:keys [recipient sender From subject
                 body-plain stripped-text stripped-signature
                 body-html stripped-html attachment-count
                 attachment-x timestamp token signature
@@ -14,3 +15,10 @@
     (logging/info "Mailgun Params: " params)
     (logging/info "Full Mailgun POST: " body)
     (str params)))
+
+(defn email-parse
+  [email]
+    (let [tasks (clojure.string/split (:body-plain email) #"/n   ")]
+      (into (empty parsed-email)
+        (for [task tasks] 
+          {:user-email (:from email) :team-email (:recipient email) :date (:subject email) :task task}))))
