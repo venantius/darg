157d3ff9655e4797837dc977025638ab7ac3b76d
johnofburnham <savelago@gmail.com>
2014-09-06 13:20:04 -0700

    Prep for email parsing

--- test/darg/fixtures/model.clj
+++ test/darg/fixtures/model.clj
@@ -5,6 +5,52 @@
   (:require [clj-time.core :as t]
             [darg.db-util :as db-util]))
 
+
+(def users-test-1
+	[  { :id 1
+	     :email "savelago@gmail.com"
+	     :username "yawn"
+	     :admin true} 
+	   { :id 2
+	     :email "domo@darg.io"
+	     :username "domodomo"
+	     :admin false}
+	   { :id 3
+	     :email "arrigato@darg.io"
+	     :username "arrigato"
+	     :admin false}
+	   { :id 4
+	     :email "butts@darg.io"
+	     :username "Mr. Butts"
+	     :admin false }]
+)
+
+(def team-test-1
+	[ {  :id 1
+	     :name "darg"}
+	  {  :id 2
+	     :name "Robtocorp"}
+	  {  :id 3
+	     :name "Jake n Cake"} ]
+)
+
+(def task-test-1
+	[{ :id 1
+	   :date (c/to-sql-date (t/date-time 2012 2 16))
+               :user-id 1
+               :team-id 1
+               :task "Do a good deed everyday"
+             }
+             {  :id 2
+                :date (c/to-sql-date (t/date-time 2012 2 16))
+                :user-id 2
+                :team-id 2
+                :task "Destroy all humans"
+            }
+             ]
+)
+>>>>>>> Prep for email parsing
+
 (def test-user-1
   {:email "savelago@gmail.com"
    :first_name "yawn"
