038b60981fcc1cc2bada646851dc9e40c4e8f11b
johnofburnham <savelago@gmail.com>
2014-09-07 15:31:12 -0700

    Early API for models
    initial user and tasks model api

--- src/darg/model/tasks.clj
+++ src/darg/model/tasks.clj
@@ -1,3 +1,35 @@
 (ns darg.model.tasks
-  (:require [darg.model :as db :only tasks]
-  	     [korma.core :refer :all]))
+  (:require [darg.model :as db]
+            [korma.core :refer :all]))
+
+
+
+;; User Tasks
+
+(defn get-all-tasks-for-user
+  [id]
+  (select db/tasks
+    (with db/users
+      (where {:user-id id}))))
+
+(defn get-user-tasks-for-daterange
+  [id minDate maxDate]
+  (select db/tasks
+    (with db/users
+      (where {:user-id id
+      	"Date is within bounds"}))))
+
+;; Team Tasks
+
+(defn get-all-tasks-for-user
+  [id]
+  (select db/tasks
+    (with db/teams
+      (where {:team-id id}))))
+
+(defn get-user-tasks-for-daterange
+  [id minDate maxDate]
+  (select db/tasks
+    (with db/users
+      (where {:team-id id
+      	"Date is within bounds"}))))
