09e9fcd6bc540a4bb5686d071ff2810113c9a6d1
johnofburnham <savelago@gmail.com>
2014-09-07 16:37:39 -0700

    Expand model API

--- src/darg/model/tasks.clj
+++ src/darg/model/tasks.clj
@@ -2,7 +2,30 @@
   (:require [darg.model :as db]
             [korma.core :refer :all]))
 
+;; Create
+(defn create-task
+  [params]
+  (insert db/tasks (values params)))
 
+(defn create-task-list
+  [tasks-list metadata]
+  (map (fn 
+         [task] 
+           (create-task 
+           (assoc metadata :task task))) 
+       tasks-list))
+
+;; Update
+
+(defn update-task
+  [id params]
+  (update db/tasks (where {:id id}) (set-fields params)))
+
+;; Destroy
+
+(defn delete-task
+  [params]
+  (delete db/tasks (where params)))
 
 ;; User Tasks
 
@@ -10,26 +33,28 @@
   [id]
   (select db/tasks
     (with db/users
-      (where {:user-id id}))))
+      (where {:id id}))))
 
-(defn get-user-tasks-for-daterange
+(defn get-tasks-for-user-daterange
   [id minDate maxDate]
   (select db/tasks
     (with db/users
-      (where {:user-id id
-      	"Date is within bounds"}))))
+      (where {:id id
+        ;Date is within bounds
+        }))))
 
 ;; Team Tasks
 
-(defn get-all-tasks-for-user
+(defn get-all-tasks-for-team
   [id]
   (select db/tasks
     (with db/teams
-      (where {:team-id id}))))
+      (where {:id id}))))
 
-(defn get-user-tasks-for-daterange
+(defn get-tasks-for-team-daterange
   [id minDate maxDate]
   (select db/tasks
     (with db/users
-      (where {:team-id id
-      	"Date is within bounds"}))))
+      (where {:id id
+        ;Date is within bounds
+        }))))
