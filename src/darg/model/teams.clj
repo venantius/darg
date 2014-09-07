67bdec6bcf4d2ad377c23d74cc6c8e9522909095
johnofburnham <savelago@gmail.com>
2014-09-07 16:37:39 -0700

    Expand model API

--- src/darg/model/teams.clj
+++ src/darg/model/teams.clj
@@ -2,3 +2,47 @@
   (:require [darg.model :as db :only teams]
                 [korma.core :refer :all]))
 
+; Create
+
+(defn create-team
+  "Insert a team into the database"
+  [params]
+  (insert db/teams (values params)))
+
+; Getters
+
+(defn get-team-by-field
+  "Find a team in the db based on a field + value"
+  [params]
+  (select db/teams (where params)))
+
+(defn get-teamid
+  "Find just the team's id based on other information"
+  [params]
+  (select db/teams (fields :id) (where params)))
+
+(defn get-team-by-id
+  "Find a team in the db based on their unique id"
+  [id]
+  (select db/teams (where {:id id})))
+
+; Update
+
+(defn update-user
+  [id params]
+  (update db/teams (where {:id id}) (set-fields params)))
+
+; Destroy
+
+(defn delete-team
+  "Delete a team from the database"
+  [params]
+  (delete db/teams (where params)))
+
+; Team Membership
+(defn get-team-users
+  "Gets the list of users for a given team"
+  [id]
+  (select db/teams 
+    (where {:id id})
+    (with db/users)))
