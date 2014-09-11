0a32dcb1da1c215ef2591e9e98c1425c0eaa59fe
David Jarvis <david@standardtreasury.com>
2014-09-11 00:08:41 -0700

    [security] Adding a session storage engine

--- src/darg/core.clj
+++ src/darg/core.clj
@@ -9,6 +9,7 @@
             [darg.init :as init]
             [darg.middleware :as middleware]
             [org.httpkit.server :as server]
+            [ring.middleware.session.cookie :as cookie]
             [ring.util.response :as resp]))
 
 ;; Pay attention to trailing slashes - right now the only thing that should end in a
@@ -16,11 +17,13 @@
 (defroutes routes
   (GET "/" [] (resp/resource-response "index.html" {:root "public"}))
   (POST "/api/v1/email" x (api/parse-forwarded-email x))
-  (route/resources "/")
-  )
+  (route/resources "/"))
 
 (def app (-> routes
-             handler/site
+             (handler/site
+               {:session {:store (cookie/cookie-store {:key "california--bear"})
+                          :cookie-attrs {:max-age 259200 ;; 3 days
+                                         }}})
              middleware/ignore-trailing-slash))
 
 (defn -main []
