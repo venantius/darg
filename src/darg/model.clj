03c58bba9c9851cfe5498b13ce1a8d46f1bab0e6
johnofburnham <savelago@gmail.com>
2014-11-28 12:58:42 -0800

    First pass at db setup
    Adds repos and github tokens tables
    
    Next questions:
    * How is user ownership set up?
    * How are tokes refreshed?

--- src/darg/model.clj
+++ src/darg/model.clj
@@ -1,14 +1,16 @@
 (ns darg.model
   (:require [korma.core :refer :all]))
 
-(declare users teams tasks)
+(declare users teams tasks repos github-token)
 
 (defentity users
   (has-many tasks)
+  (has-one github-token)
   (many-to-many teams :team_users))
 
 (defentity teams
   (has-many tasks)
+  (has-many repos :team_repos)
   (many-to-many users :team_users))
 
 (defentity team-users
@@ -23,3 +25,13 @@
 (defentity password-reset-tokens
   (table :password_reset_tokens)
   (belongs-to users {:FK :users_id}))
+
+(defentity repos 
+  (many-to-many teams :team_repos))
+
+(defentity github-token
+  (belongs-to users))
+
+(defentity team-repos
+  (table :team_repos)
+  (has-many teams {:FK :teams_id}))
