5a73e0acc1bf60565cf4a1e3339066ee1b5d9341
johnofburnham <savelago@gmail.com>
2014-11-29 15:42:18 -0800

    Fixes
    * Pluralization
    * Reorganized GH Event ownership to belong to a gh user
    * Split tokens into a separate table
    * Added some fields to Github-users
    * Formatting cleanup

--- src/lobos/migrations.clj
+++ src/lobos/migrations.clj
@@ -10,126 +10,138 @@
 ;; NOTE: Found this a handy reference on Lobos
 ;; http://vijaykiran.com/2012/01/web-application-development-with-clojure-part-2/
 
-(defmigration add-users-table
+(defmigration add-github-tokens-table
   (up [] (create
-           (table :users
-                  (integer :id :auto-inc :primary-key)
-                  (text :email :unique :not-null)
-                  (text :password :not-null)
-                  (text :name :not-null)
-                  (boolean :admin (default false))
-                  (boolean :bot (default true))
-                  (boolean :active (default false)))))
-  (down [] (drop (table :users))))
+           (table :github_tokens
+           (integer :id :primary-key)
+           (text :gh_token :not-null)
+           (boolean :repo_scope (default false)))))
+  (down [] (drop (table :github_tokens))))
 
 (defmigration add-github-users-table
   (up [] (create
-            (table :github_users
-              (integer :id :primary-key)
-              (integer :users_id [:refer :users :id :on-delete :cascade] :not-null)
-              (text :gh_access_token :not-null)
-              (boolean :repo_scope (default false)))))
+           (table :github_users
+             (integer :id :primary-key)
+             (text :github_username)
+             (text :github_email)
+             (text :github_gravatar_url)
+             (integer :github_tokens_id [:refer :github_tokens :id :on-delete :set-null]))))
   (down [] (drop (table :github_users))))
+
+(defmigration add-users-table
+  (up [] (create
+           (table :users
+             (integer :id :auto-inc :primary-key)
+             (text :email :unique :not-null)
+             (text :password :not-null)
+             (text :name :not-null)
+             (boolean :admin (default false))
+             (boolean :bot (default true))
+             (integer :github_users_id [:refer :github_users :id])
+             (boolean :active (default false)))))
+  (down [] (drop (table :users))))
 
 (defmigration add-teams-table
   (up [] (create
            (table :teams
-                  (integer :id :auto-inc :primary-key)
-                  (text :name :not-null)
-                  (text :email :not-null :unique))))
+             (integer :id :auto-inc :primary-key)
+             (text :name :not-null)
+             (text :email :not-null :unique))))
   (down [] (drop (table :teams))))
 
 (defmigration add-tasks-table
   (up [] (create
            (table :tasks
-                  (integer :id :auto-inc :primary-key)
-                  (timestamp :date :not-null)
-                  (integer :users_id [:refer :users :id :on-delete :set-null])
-                  (integer :teams_id [:refer :teams :id :on-delete :cascade] :not-null)
-                  (text :task))))
+             (integer :id :auto-inc :primary-key)
+             (timestamp :date :not-null)
+             (integer :users_id [:refer :users :id :on-delete :set-null])
+             (integer :teams_id [:refer :teams :id :on-delete :cascade] :not-null)
+             (text :task))))
   (down [] (drop (table :tasks))))
 
 (defmigration add-github-repos-table
   (up [] (create 
-             (table :github_repos
-                (integer :id :primary-key)
-                (text :name :not-null)
-                (text :description)
-                (text :html-url :not-null))))
+           (table :github_repos
+             (integer :id :primary-key)
+             (text :name :not-null)
+             (text :description)
+             (text :html-url :not-null))))
   (down [] (drop (table :github_repos))))
 
 (defmigration add-team-users-table
   (up [] (create
            (table :team_users
-                  (integer :id :auto-inc :primary-key)
-                  (text :role)
-                  (integer :users_id [:refer :users :id :on-delete :cascade] :not-null)
-                  (integer :teams_id [:refer :teams :id :on-delete :cascade] :not-null)
-                  (boolean :admin (default false)))))
+             (integer :id :auto-inc :primary-key)
+             (text :role)
+             (integer :users_id [:refer :users :id :on-delete :cascade] :not-null)
+             (integer :teams_id [:refer :teams :id :on-delete :cascade] :not-null)
+             (boolean :admin (default false)))))
   (down [] (drop (table :team_users))))
 
 (defmigration add-team-repos-table
   (up [] (create
-             (table :team_repos
-                (integer :id :auto-inc :primary-key)
-                (integer :github_repos_id [:refer :github_repos :id :on-delete :cascade] :not-null)
-                (integer :teams_id [:refer :teams :id :on-delete :cascade])
-                (text :status (default "active") :not-null))))
+           (table :team_repos
+             (integer :id :auto-inc :primary-key)
+             (integer :github_repos_id [:refer :github_repos :id :on-delete :cascade] :not-null)
+             (integer :teams_id [:refer :teams :id :on-delete :cascade])
+             (text :status (default "active") :not-null))))
   (down [] (drop (table :team_repos))))
 
-(defmigration add-gh-push-table
+(defmigration add-github-pushes-table
   ; see https://developer.github.com/v3/activity/events/types/#pushevent
+  ; Desire is to create the message: "$USER pushed $HEAD-MESSAGE and ($SIZE - 1) other commits to $REF"
   (up [] (create
-             (table :gh_push
-                (integer :id :auto-inc :primary-key)
-                (integer :users_id [:refer :users :id :on-delete :set-null])
-                (integer :github_repos_id [:refer :github_repos :id :on-delete :cascade] :not-null)
-                (integer :size :not-null)
-                (text :ref :not-null)
-                (text :head-commit-message :not-null)
-                (text :compare-url :not-null)
-                (timestamp :timestamp :not-null))))
-  (down [] (drop (table :gh_push))))
+           (table :github_pushes
+             (integer :id :auto-inc :primary-key)
+             (integer :github_users_id [:refer :github_users :id :on-delete :set-null])
+             (integer :github_repos_id [:refer :github_repos :id :on-delete :cascade] :not-null)
+             (integer :size :not-null) ;number of commits in push
+             (text :ref :not-null) ;full git ref that was pushed (repo + branch)
+             (text :head-commit-message :not-null) ;message on the top commit
+             (text :compare-url :not-null)
+             (timestamp :timestamp :not-null))))
+  (down [] (drop (table :github_pushes))))
 
-(defmigration add-gh-issue-table
+(defmigration add-github-issues-table
+  ; Desire is to create the message: "$USER $ACTIONED issues $TITLE"
   (up [] (create
-             (table :gh_issue
-                (integer :id :auto-inc :primary-key)
-                (integer :users_id [:refer :users :id :on-delete :set-null])
-                (integer :github_repos_id [:refer :github_repos :id :on-delete :cascade] :not-null)
-                (text :action :not-null)
-                (integer :number :not-null)
-                (text :title :not-null)
-                (text :url :not-null)
-                (timestamp :timestamp :not-null))))
-  (down [] (drop (table :gh_issue))))
+           (table :github_issues
+             (integer :id :auto-inc :primary-key)
+             (integer :github_users_id [:refer :github_users :id :on-delete :set-null])
+             (integer :github_repos_id [:refer :github_repos :id :on-delete :cascade] :not-null)
+             (text :action :not-null)
+             (integer :number :not-null)
+             (text :title :not-null)
+             (text :url :not-null)
+             (timestamp :timestamp :not-null))))
+  (down [] (drop (table :github_issues))))
 
-(defmigration add-gh-pullrequest-table
+(defmigration add-github-pullrequests-table
   (up [] (create 
-              (table :gh_pullrequest
-                (integer :id :auto-inc :primary-key)
-                (integer :users_id [:refer :users :id :on-delete :set-null])
-                (integer :github_repos_id [:refer :github_repos :id :on-delete :cascade] :not-null)
-                (text :action :not-null)
-                (integer :number :not-null)
-                (text :title :not-null)
-                (text :url :not-null)
-                (timestamp :timestamp :not-null))))
-  (down [] (drop (table :gh_pullrequest))))
+           (table :github_pullrequests
+             (integer :id :auto-inc :primary-key)
+             (integer :github_users_id [:refer :github_users :id :on-delete :set-null])
+             (integer :github_repos_id [:refer :github_repos :id :on-delete :cascade] :not-null)
+             (text :action :not-null)
+             (integer :number :not-null)
+             (text :title :not-null)
+             (text :url :not-null)
+             (timestamp :timestamp :not-null))))
+  (down [] (drop (table :github_pullrequests))))
 
 (defmigration add-api-keys-table
   (up [] (create
            (table :api_keys
-                  (integer :id :auto-inc :primary-key)
-                  (text :api-key :unique :not-null)
-                  (integer :users_id [:refer :users :id :on-delete :cascade] :not-null))))
+             (integer :id :auto-inc :primary-key)
+             (text :api-key :unique :not-null)
+             (integer :users_id [:refer :users :id :on-delete :cascade] :not-null))))
   (down [] (drop (table :api_keys))))
 
 (defmigration add-password-reset-tokens-table
   (up [] (create
            (table :password_reset_tokens
-                  (integer :id :auto-inc :primary-key)
-                  (text :token :unique :not-null)
-                  (integer :users_id [:refer :users :id :on-delete :cascade] :not-null)
-                  (timestamp :expires_at :not-null (default (now))))))
+             (integer :id :auto-inc :primary-key)
+             (text :token :unique :not-null)
+             (integer :users_id [:refer :users :id :on-delete :cascade] :not-null)
+             (timestamp :expires_at :not-null (default (now))))))
   (down [] (drop (table :password_reset_tokens))))
