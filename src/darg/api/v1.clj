53cf19ab0fe4339aff9cd7f67cfdf8f90329e61a
David Jarvis <david@standardtreasury.com>
2015-03-31 11:13:29 -0700

    [api] Even more controller refactoring.

--- src/darg/api/v1.clj
+++ src/darg/api/v1.clj
@@ -1,72 +1,11 @@
 (ns darg.api.v1
-  (:require [clojure.tools.logging :as logging]
-            [clojure.string :as str :refer [split trim]]
-            [darg.api.responses :as responses]
+  (:require [darg.api.responses :as responses]
+            [darg.controller.auth :as auth-api]
+            [darg.controller.email :as email-api]
             [darg.controller.gravatar :as gravatar-api]
             [darg.controller.task :as task-api]
             [darg.controller.user :as user-api]
-            [darg.db-util :as dbutil]
-            [darg.model.darg :as darg]
-            [darg.model.email :as email]
-            [darg.model.task :as task]
-            [darg.model.user :as user]
-            [darg.services.mailgun :as mailgun]
-            [korma.core :refer :all]
-            [korma.sql.fns :as ksql]))
-
-(defn login
-  "/api/v1/login
-
-  Method: POST
-
-  Authentication endpoint. if successful, we set auth in their session and
-  update the cookie to indicate that they're now logged in."
-  [{:keys [params] :as request}]
-  (let [{:keys [email password]} params]
-    (cond
-      (not (user/authenticate email password))
-      {:body "Failed to authenticate"
-       :session {:authenticated false}
-       :status 401}
-      :else
-      (let [id (:id (user/fetch-one-user {:email email}))]
-        {:body "Successfully authenticated"
-         :cookies {"logged-in" {:value true :path "/"}
-                   "id" {:value id :path "/"}}
-         :session {:authenticated true :id id :email email}
-         :status 200}))))
-
-(defn logout
-  "/api/v1/logout
-
-  Method: GET
-
-  The other half of the authentication endpoint pair. This one clears
-  your session cookie and your plaintext cookie, logging you out both
-  in practice and appearance"
-  [request-map]
-  {:body ""
-   :status 200
-   :session nil
-   :cookies {"logged-in" {:value false :max-age 0 :path "/"}}})
-
-(defn password-reset
-  "/api/v1/password_reset
-
-  Method: POST
-
-  Initiates the password reset workflow. This will send an e-mail to the user
-  with a special link that they can use to reset their password. The link will
-  only remain valid for a finite amount of time."
-  [request-map]
-  (let [email (-> request-map :params :email)]
-    (try
-      (user/send-password-reset-email email)
-      (responses/ok "Success!")
-      (catch Exception e
-        (responses/bad-request "Password reset failed.")))))
-
-(def signup user-api/create!)
+            [darg.model.darg :as darg]))
 
 (def update-user user-api/update!)
 
@@ -99,29 +38,7 @@
     (responses/ok
      {:darg (darg/team-timeline team-id)})))
 
-(defn email
-  "/api/v1/email
+(def email email-api/email)
 
-  E-mail parsing endpoint; only for use with Mailgun. Authenticates the e-mail
-  from Mailgun, and adds a task for each newline in the :stripped-text field."
-  [{:keys [params]}]
-  (let [{:keys [recipient sender from subject
-                body-plain stripped-text stripped-signature
-                body-html stripped-html attachment-count
-                attachment-x timestamp token signature
-                message-headers content-id-map] :as email} params]
-    (try
-      (cond
-        (not (mailgun/authenticate email))
-        (responses/unauthorized "Failed to authenticate email.")
-        (not (email/user-can-email-this-team? from recipient))
-        (responses/unauthorized (format "E-mails from this address <%s> are not authorized to post to this team address <%s>." from recipient))
-        :else
-        (do
-          (email/parse-email email)
-          (responses/ok {:message "E-mail successfully parsed."})))
-      (catch Exception e
-        (logging/errorf "Failed to parse email with exception: %s" e)
-        (responses/bad-request "Failed to parse e-mail.")))))
 
 (def get-user user-api/get)
