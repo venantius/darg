31733d00ba5f70d362197f4de1ab9930bb31f806
David Jarvis <david@standardtreasury.com>
2015-04-01 12:43:10 -0700

    [api] Adding task update, task controller tests
    Also modifying logging to be less verbose.

--- src/darg/controller/task.clj
+++ src/darg/controller/task.clj
@@ -12,7 +12,7 @@
 
   Create a task."
   [{:keys [params user] :as request}]
-  (log/info params)
+  (log/info "Creating task:" params)
   (let [task (:task params)
         user-id (:id user)
         team-id (read-string (:team_id params))
@@ -23,10 +23,10 @@
       (not (user/user-in-team? user-id team-id))
       (responses/unauthorized "Not authorized.")
       :else
-      (task/create-task! {:task task
-                          :user_id user-id
-                          :team_id team-id
-                          :date date}))))
+      (responses/ok (task/create-task! {:task task
+                                        :user_id user-id
+                                        :team_id team-id
+                                        :date date})))))
 
 (defn update!
   "/api/v1/task/:id
@@ -34,8 +34,15 @@
    Method: POST
    
    Update an existing task."
-  [{:keys [params user]}]
-  (log/info "Updating task" params)
-  ()
-
-  )
+  [{:keys [params user] :as request}]
+  (log/info "Updating task:" request)
+  (let [new-task (:task params)
+        user-id (:id user)
+        id (-> params :id read-string)
+        old-task (task/fetch-one-task {:id id})]
+    (cond
+      (not= (:user_id old-task) user-id)
+      (responses/unauthorized "Not authorized.")
+      :else
+      (responses/ok (task/update-task! id {:task new-task
+                                           :id id})))))
