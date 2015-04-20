2ea2c6d63c2f8599bb0f2ef133910042d8b9e7ec
David Jarvis <david@standardtreasury.com>
2015-04-20 15:21:34 -0700

    [dockercfg] Move the .dockercfg over to $HOME/.dockercfg
    Apparently the docker client is smart enough to know it exists if it's
    in the current dir but not actually to do anything with it. Stupid.

--- scripts/quay-deploy.sh
+++ scripts/quay-deploy.sh
@@ -2,6 +2,6 @@
 
 lein uberjar
 
-sed "s#<QUAY_AUTH>#$QUAY_AUTH#" < .dockercfg.template > .dockercfg
+sed "s#<QUAY_AUTH>#$QUAY_AUTH#" < .dockercfg.template > $HOME/.dockercfg
 docker build -t quay.io/venantius/darg .
 docker push quay.io/venantius/darg
