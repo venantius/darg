7b00df6658919c5bb4478bfd55859ea4bef9913c
David Jarvis <david@standardtreasury.com>
2015-04-20 14:48:46 -0700

    [quay] Oops

--- scripts/quay-deploy.sh
+++ scripts/quay-deploy.sh
@@ -4,4 +4,4 @@ lein uberjar
 
 sed "s#<QUAY_AUTH>#$QUAY_AUTH#" < .dockercfg.template > .dockercfg
 docker build -t quay.io/venantius/darg .
-docker push quay.io/mynamespace/myreponame
+docker push quay.io/venantius/darg
