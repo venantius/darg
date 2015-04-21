812ac8f8d07f83b978489cad9899c8441155ff07
David Jarvis <david@standardtreasury.com>
2015-04-20 23:50:21 -0700

    [quay] more config

--- scripts/quay-deploy.sh
+++ scripts/quay-deploy.sh
@@ -3,9 +3,10 @@
 lein uberjar
 
 export NOW=$(date +%s)
+export DOCKER_TAG=quay.io/venantius/darg:$NOW-${CIRCLE_SHA1:0:8}
 
 sed "s#<QUAY_AUTH>#$QUAY_AUTH#" < .dockercfg.template > $HOME/.dockercfg
-docker build -t quay.io/venantius/darg .
-docker tag -f quay.io/venantius/darg:$NOW-${CIRCLE_SHA1:0:8} quay.io/venantius/darg:latest
-docker push quay.io/venantius/darg:$NOW-${CIRCLE_SHA1:0:8}
+docker build -t $DOCKER_IMAGE .
+docker tag -f $DOCKER_IMAGE quay.io/venantius/darg:latest
+docker push $DOCKER_IMAGE
 docker push quay.io/venantius/darg:latest
