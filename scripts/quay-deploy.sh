7e0e5b092b89baaf17795a4fa7178ec275f2c596
David Jarvis <david@standardtreasury.com>
2015-04-21 00:02:10 -0700

    [scripts]

--- scripts/quay-deploy.sh
+++ scripts/quay-deploy.sh
@@ -6,7 +6,7 @@ export NOW=$(date +%s)
 export DOCKER_TAG=quay.io/venantius/darg:$NOW-${CIRCLE_SHA1:0:8}
 
 sed "s#<QUAY_AUTH>#$QUAY_AUTH#" < .dockercfg.template > $HOME/.dockercfg
-docker build -t $DOCKER_IMAGE .
-docker tag -f $DOCKER_IMAGE quay.io/venantius/darg:latest
-docker push $DOCKER_IMAGE
+docker build -t ${DOCKER_TAG} .
+docker tag -f ${DOCKER_TAG} quay.io/venantius/darg:latest
+docker push ${DOCKER_TAG}
 docker push quay.io/venantius/darg:latest
