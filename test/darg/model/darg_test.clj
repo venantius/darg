d9dfcadc46b6cef0b3d4b27e1d0cf145ab8b9814
David Jarvis <david@standardtreasury.com>
2015-04-30 23:31:31 -0700

    [tests] Timezone bit me in the butt on tests

--- test/darg/model/darg_test.clj
+++ test/darg/model/darg_test.clj
@@ -1,17 +1,23 @@
 (ns darg.model.darg-test
   (:require [clj-time.coerce :as c]
             [clj-time.core :as t]
+            [clj-time.format :as f]
+            [clj-time.local :as l]
             [clojure.test :refer :all]
+            [clojure.tools.logging :as log]
             [darg.fixtures :refer [with-db-fixtures]]
             [darg.fixtures.model :as fixture-data]
             [darg.model.darg :as darg]
-            [darg.util :as util]))
+            [darg.util :as util]
+            [darg.util.datetime :as dt]))
 
 (with-db-fixtures)
 
 (deftest team-timeline-works
-  (let [task-date (c/from-sql-time 
-                    (:timestamp fixture-data/test-task-1))
+  (let [task-date (dt/local-time
+                    (c/from-sql-time 
+                      (:timestamp fixture-data/test-task-1))
+                    (:timezone fixture-data/test-user-4))
         year (t/year task-date)
         month (t/month task-date)
         day (t/day task-date)]
@@ -40,7 +46,7 @@
                    :name "Dave"
                    :email "venantius@gmail.com"
                    :id 7}]
-          :date (util/sql-datetime->date-str (:timestamp fixture-data/test-task-1))}
+          :date (l/format-local-time task-date :date)}
           (first (darg/team-timeline 
                    fixture-data/test-user-4 1 
                    (t/date-time year month day)))))))
