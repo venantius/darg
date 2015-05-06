00489c144fd79dfadb08a1e95af7ea788eafa0c3
David Jarvis <david@standardtreasury.com>
2015-05-06 14:43:16 -0700

    WIP

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,24 @@
 module.exports = function(grunt) {
   grunt.initConfig({
-    // running `grunt concat` will concat our source js
+
+    // `grunt concat` to concat our source
     concat: {
-      dist: {
+
+      // the angular app
+      basic: {
         src: './src/js/**/*.js',
         dest: './resources/public/js/darg/app.js'
+      },
+
+      // all of our html templates for uncss, etc.
+      extras: {
+        src: ['./resources/build/raw/index.html', 
+              './resources/public/templates/**/*.html'],
+        dest: './resources/build/concat/templates.html'
       }
     },
 
-    // running `grunt less` will compile our less files
+    // `grunt less` to compile our css files
     less: {
       development: {
         options: {
@@ -22,21 +32,57 @@ module.exports = function(grunt) {
       }
     },
 
-    // `grunt uncss` to compile a minified, JIT css file for email
+    // `grunt uncss` to compile a CSS file for only the required CSS
     uncss: {
       dist: {
+        options: {
+          // don't try to get around the CDN
+          ignoreSheets: [/cdnjs/, /maxcdn/],
+          timeout: 1000,
+        },
         files: {
-          "./resources/email/css/email.css":
-            ['./resources/email/templates/raw/digest.html']
+          // email css
+          "./resources/email/css/email.css": 
+            './resources/email/templates/raw/digest.html',
+
+          // www css
+          "./resources/public/css/darg.css": 
+            "./resources/build/concat/templates.html",
+        }
+      },
+
+      web: {
+        options: {
+          // don't try to get around the CDN
+          ignoreSheets: [/cdnjs/, /maxcdn/],
+          timeout: 1000,
+        },
+        files: {
+          // email css
+          "./resources/email/css/email.css": 
+            './resources/email/templates/raw/digest.html',
+
+          // www css
+          "./resources/public/css/darg.css": 
+            "./resources/build/concat/templates.html",
         }
       }
+
     },
 
     // replace stylesheet with economy version
     processhtml: {
-      dist: {
+      email: {
         files: {
-          'resources/email/templates/processed/digest.html': ['resources/email/templates/raw/digest.html']
+          // daily digest email
+          'resources/email/templates/processed/digest.html': 
+            ['resources/email/templates/raw/digest.html'],
+        }
+      },
+      web: {
+        files: {
+          'resources/public/index.html':
+            ['resources/build/raw/index.html'],
         }
       }
     },
@@ -66,6 +112,10 @@ module.exports = function(grunt) {
       email: {
         files: ["./resources/email/templates/**/*.html", "./src/less/**/*.less"],
         tasks: ["uncss", "processhtml", "premailer"]
+      },
+      web: {
+        files: ["./resources/**/*.html"],
+        tasks: ["concat", "uncss"]
       }
     }
   });
