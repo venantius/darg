118a8abf0910c20bfa9c4c486fb0af9a863aa245
David Jarvis <david@standardtreasury.com>
2015-05-08 12:19:15 +0900

    [frontend] Removing a debug line from the Settings controller

--- resources/public/js/darg/app.js
+++ resources/public/js/darg/app.js
@@ -331,7 +331,6 @@ darg.controller('DargSettingsCtrl',
         return user.info
     }, function(newValue, oldValue) {
         self.userProfile = newValue;
-        console.log(newValue);
     });
 
 }]);
